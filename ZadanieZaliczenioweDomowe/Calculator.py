809f34857e0590f9dde9ec6d22166b3ee457ce76
Mykola9518 <nikolay.lozyuk.95@gmail.com>
2025-01-05 16:12:01 +0100

    CalcVer1

---
+++ ZadanieZaliczenioweDomowe/Calculator.py
@@ -0,0 +1,81 @@
+﻿def power(a,b):
+    res = 1
+    for i in range(b):
+        res *= a
+    return res
+def factorial(a):
+    if a == 0:
+        return 1
+    elif a > 0:
+        return a*factorial(a-1)
+    else:
+        raise ValueError('Must be more than 0')
+
+import os
+isnotcorrect = True
+result = 0
+while isnotcorrect:
+    print('\n\n\t Calculator Mode')
+    try:
+            print('Possible operations')
+            operation_desc = [
+            'q -- quit calculator mode',
+            '+ -- addition operation',
+            '- -- subtraction opration',
+            '* -- multiplication operation',
+            '/ -- division operation',
+            '! -- factorial operation',
+            '^ -- operation of reduction to power'
+            ]
+            operations = ['q','+','-','*','/','!','^']
+            for item in operation_desc:
+                print(item)
+            print('Enter an operation')
+            c=str(input())
+            if c in operations:
+                if c == 'q':
+                        print('Quit from calculator')
+                        isnotcorrect = False
+                elif c == '!':
+                        print('Enter value')
+                        a=int(input())
+                        result = factorial(a)
+                        print(a,c,'=',result)
+                        isnotcorrect = True
+                else:
+                    print('Enter first value')
+                    a=float(input())
+                    print('Enter second value')
+                    b=float(input())
+                
+                    if c == '+':
+                            result = a+b
+                            print(a,c,b,'=',result)
+                    elif c == '-':
+                            result = a-b
+                            print(a,c,b,'=',result)
+                    elif c == '*':
+                            result = a*b
+                            print(a,c,b,'=',result)
+                    elif c == '/':
+                            result = a/b
+                            print(a,c,b,'=',result)
+                    elif c == '^':
+                            result = power(a,int(b))
+                            print(a,c,int(b),'=',result)
+                    
+            else:
+                raise ValueError('Not corrected operation')
+    except ValueError as e:
+            print(f"Error: {e}")
+            isnotcorrect = True
+            os.system('cls')
+    except ZeroDivisionError:
+        print('Can not division operation by 0')
+        isnotcorrect = True
+        os.system('cls')
+
+      
+        
+
+        
